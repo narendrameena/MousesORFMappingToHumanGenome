b4c898635349ab4fdaed154ea72ab9402ffea4e9
Narendra Meena <narendrabhagri03@gmail.com>
2017-11-06 21:13:14 +0530

    write results in a file

--- mapMuatationsToSORFsWIthParrallel.R
+++ mapMuatationsToSORFsWIthParrallel.R
@@ -18,21 +18,23 @@ sORFs <- read.delim("data/mappedHg38MouseSOrfs.txt", header = FALSE)
 
 #head(sORFs )
 
-mutation$X.CHROM[1]
+#mutation$X.CHROM[1]
 
 
 #function in R 
 
 mapMutTOsORF <- function(i){
+  print(i)
   for (j in 1:length(sORFs$V1)){
     if( mutation$X.CHROM[i] %in% sORFs$V1[j]){
-      #print(mutation$X.CHROM[i])
-      #print(sORFs$V1[i])
-      #print(sORFs$V2[i])
-      #print(mutation$POS[i])
-      #print(sORFs$V3[i])
+      print(mutation$X.CHROM[i])
+      print(sORFs$V1[i])
+      print(sORFs$V2[i])
+      print(mutation$POS[i])
+      print(sORFs$V3[i])
       if(sORFs$V2[j] <= mutation$POS[i] & mutation$POS[i]<= sORFs$V3[j]){
-        print(paste(sORFs$V2[j] , mutation$POS[i] , sORFs$V3[j] , sep = ","))
+        print(paste(mutation$X.CHROM[i], sORFs$V1[j], sORFs$V2[j] , mutation$POS[i] , sORFs$V3[j] , sep = ","))
+        return(paste(mutation$X.CHROM[i], sORFs$V1[j], sORFs$V2[j] , mutation$POS[i] , sORFs$V3[j] , sep = ","))
         
       }
     }
@@ -45,12 +47,15 @@ cores=detectCores()
 cl <- makeCluster(cores[1]-1) #not to overload your computer
 registerDoParallel(cl)
 
-mappedMutations <- foreach(i=1:length(mutation$X.CHROM, .combine=cbind) %dopar% {
+
+
+foreach(i=1:length(mutation$X.CHROM), .combine=cbind) %dopar% {
   tempMatrixOfMutMappedTosORFs = mapMutTOsORF(i)
-    #calling a function
-  tempMatrixOfMutMappedTosORFs
-}
-  #print(i)
-)
+  
+  ## write results to disk
+  file=paste0(tempMatrixOfMutMappedTosORFs,"/results_",i,".csv")
+  write.csv(results,file=file)
+  return(NULL)
+  }
 #stop cluster
 stopCluster(cl)
